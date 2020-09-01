23a34aa5b54ed436c925fa9386a3749d9fbc6c58
Cristian Suárez <cristiansuarezg7@gmail.com>
2020-09-01 08:40:41 +0200

    promises

--- index.js
+++ index.js
@@ -1,79 +1,105 @@
 const XLSX = require('xlsx');
 const path = require('path');
 const fs = require('fs');
+const { resolve } = require('path');
 
-const workbook = XLSX.readFile(path.join(__dirname, 'inputs', 'articulos.xlsx'));
+const workbook = XLSX.readFile(
+  path.join(__dirname, 'inputs', 'articulos.xlsx'),
+);
 
 const workbookJSON = workbook => {
-    const firstSheet = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[firstSheet];
+  const firstSheet = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[firstSheet];
 
-    return sheetJSON = XLSX.utils.sheet_to_json(worksheet, {raw: true, defval:null});
-}
-
-
+  return (sheetJSON = XLSX.utils.sheet_to_json(worksheet, {
+    raw: true,
+    defval: null,
+  }));
+};
 
 // Setup:
-const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+const WooCommerceRestApi = require('@woocommerce/woocommerce-rest-api').default;
 
 const WooCommerce = new WooCommerceRestApi({
   url: 'https://supermercadovidemasca.es/', // Your store URL
   consumerKey: 'ck_1a1d7c1c59d04169468d2391fcbf7ca029b3af10', // Your consumer key
   consumerSecret: 'cs_68fa64c3e0cfff0b45f3947be6a90424d789f663', // Your consumer secret
   version: 'wc/v3', // WooCommerce WP REST API version
-  queryStringAuth: true //Server SSL config
+  queryStringAuth: true, //Server SSL config
 });
 
-
 const addProduct = (workbook, index) => {
-    const data = {
-        name: workbook[index].Descripcion,
-        type: "simple",
-        regular_price: workbook[index]['PVP 1'].toString(),
-        categories: [
-            {
-              id: 1
-            },
-        ],
-    }
+  const data = {
+    name: workbook[index].Descripcion,
+    type: 'simple',
+    regular_price: workbook[index]['PVP 1'].toString(),
+    categories: [
+      {
+        id: 1,
+      },
+    ],
+  };
 
-    WooCommerce.post("products", data)
-    .then((response) => {
-        console.log(response.data);
+  WooCommerce.post('products', data)
+    .then(response => {
+      console.log(response.data);
     })
-    .catch((error) => {
-        console.log(error.response.data);
+    .catch(error => {
+      console.log(error.response.data);
     });
-}
+};
 
-const addProducts = async (workbook) => {
+const addProducts = async workbook => {
+  const send = await Promise.all(
+    workbook.map(element => {
+      const data = {
+        name: element.Descripcion,
+        type: 'simple',
+        regular_price: element['PVP 1'].toString(),
+        categories: [
+          {
+            id: 1,
+          },
+        ],
+      };
 
-    const send = await Promise.all(
-        workbook.map(element => {
-            const data = {
-                name: element.Descripcion,
-                type: "simple",
-                regular_price: element['PVP 1'].toString(),
-                categories: [
-                    {
-                      id: 1
-                    },
-                ],
-            }
+      console.log(data);
 
-            console.log(data)
+      // WooCommerce.post("products", data)
+      // .then((response) => {
+      //     console.log(response.data);
+      // })
+      // .catch((error) => {
+      //     console.log(error.response.data);
+      // });
+    }),
+  );
+};
 
-            // WooCommerce.post("products", data)
-            // .then((response) => {
-            //     console.log(response.data);
-            // })
-            // .catch((error) => {
-            //     console.log(error.response.data);
-            // });
-        })
-    )
+const returnData = workbook => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(workbook);
+      console.log('Getting the data');
+    }, 2000);
+  });
+};
 
-}
+// const returnAllData = (workbook) => {
+//   return new Promise((resolve, reject) => {
+//     returnData(workbookJSON(workbook), 5).then(data => resolve(data));
+//   });
+// };
+
+const promises = workbookJSON(workbook).map(async item => {
+  const response = await returnData(item);
+
+  return response;
+});
+
+Promise.all(promises).then(value => console.log(value));
+
+// const returnAllData = returnAllData(workbookJSON(workbook));
 
 // addProduct(workbookJSON(workbook), 5);
-await addProducts(workbookJSON(workbook))
+// await addProducts(workbookJSON(workbook));
